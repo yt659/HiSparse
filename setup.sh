fc58bbbc3279f23bb6b47a488edd68083fd118b0
Yixiao Du <yd383@cornell.edu>
2024-09-22 17:22:35 -0400

    setting up svpp

---
+++ setup.sh
@@ -0,0 +1,18 @@
+echo setting up Vitis-2022.1 environment ...
+unset LM_LICENSE_FILE
+export XILINXD_LICENSE_FILE=2100@flex.ece.cornell.edu
+source scl_source enable devtoolset-10
+source /opt/xilinx/2022.1/Vitis/2022.1/settings64.sh > /dev/null
+source /opt/xilinx/xrt/setup.sh > /dev/null
+export HLS_INCLUDE=/opt/xilinx/2022.1/Vitis_HLS/2022.1/include
+export LD_LIBRARY_PATH=/opt/xilinx/2022.1/Vitis/2022.1/lib/lnx64.o:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/xilinx/2022.1/Vitis/2022.1/lib/lnx64.o/Default:$LD_LIBRARY_PATH
+echo Vitis-2022.1 setup finished
+echo setting up svpp ...
+export PATH=$PWD/svpp:$PATH
+echo svpp setup finished
+
+echo setup finished:
+echo " v++ is: $(which v++)"
+echo " xbutil is: $(which xbutil)"
+echo " svpp is: $(which svpp)"
