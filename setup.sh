2a9f07fcbf0063d7dc8b5d009e190650f4fa23b6
Yixiao Du <yd383@cornell.edu>
2024-09-22 18:31:52 -0400

    HLS library doesn't compile with gcc10, going back to gcc8

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 echo setting up Vitis-2022.1 environment ...
 unset LM_LICENSE_FILE
 export XILINXD_LICENSE_FILE=2100@flex.ece.cornell.edu
-source scl_source enable devtoolset-10
+source scl_source enable devtoolset-8
 source /opt/xilinx/2022.1/Vitis/2022.1/settings64.sh > /dev/null
 source /opt/xilinx/xrt/setup.sh > /dev/null
 export HLS_INCLUDE=/opt/xilinx/2022.1/Vitis_HLS/2022.1/include
